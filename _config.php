53e93e4ff67efe5e67e8cc9d263d33380642d664
Jotham Read <jotham.read@gmail.com>
2014-11-13 15:46:45 +1300

    Fixed a couple of things

--- _config.php
+++ _config.php
@@ -22,20 +22,45 @@ function trace(){
  *
  */
 
-function tracef($metadata=null, $depth=null){
-   if ( is_null($depth) ) $depth = 1;
+ // Traces the call stack
+ function traces($metadata=null, $depth=null, $file=false){
+   if ( is_int($metadata) && is_null($depth) ) { $depth = $metadata; $metadata = null; }
+   if ( is_null($depth) ) { $depth = 1; } else { $depth += 1; }
    $backtrace = debug_backtrace(true, $depth+1);
    for ($i=1; $i < count($backtrace); ++$i){
       $obj = $backtrace[$i];
       $args = array();
       if ( $depth > 1 && $i > 1 ) $args[] = $i;
       if ( isset($obj['class']) ) {
-         $args[] = $obj['class'] . $obj['type']. $obj['function'];
+         $args[] = (isset($obj['file'])?$file?$obj['file'].': ':'':'') . $obj['class'] . $obj['type']. $obj['function'];
       }
       $args[] = $obj['args'];
       if ( $i == 1 && !is_null($metadata) ) $args[] = $metadata;
       error_log(($i>1?' ':'').__buildStr($args));
    }
+ }
+
+// Traces passed arguments with 1 level of the call stack as a prefix
+//TODO: Refactor both of these
+function tracef(){
+   $backtrace = debug_backtrace(true, 2);
+   $obj = end($backtrace);
+   if ( isset($obj['class']) ) {
+      $fname = $obj['class'] . $obj['type']. $obj['function'];
+   } else {
+      $fname = 'WARN:' . __buildStr($obj);
+   }
+   $parts = array($fname, __buildStr($obj['args']));
+   if ( func_num_args() > 0 ) {
+      // metadata
+      $args = func_get_args();
+      $parts[] = (func_num_args() == 1) ? $args[0] : __buildStr($args);
+   }
+   error_log(implode(' ', $parts));
+}
+
+function tracefp($metadata=null, $depth=null){
+   tracef($metadata, $depth, true);
 }
 
 /*
@@ -63,8 +88,8 @@ function __buildStr($args, $depth=0, $max_depth=5){
       }*/
    }
    $output = str_replace('\\', '', implode(" ", $result));
-   if ( in_array(substr($output, 0, 1), array("{","[")) ) {
+   /*if ( in_array(substr($output, 0, 1), array("{","[")) ) {
       $output = substr($output, 1, count($output)-2);
-   }
+   }*/
    return $output;
 }
